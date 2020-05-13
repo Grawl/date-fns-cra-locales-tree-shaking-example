4bcc084a36166af4ff88120e5cf6a8a3ea6f1f29
Даниил Пронин <mail@grawl.ru>
2020-05-13 20:04:41 +1000

    broke Storybook with Dart Sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        sassLoaderOptions: {
+          implementation: require("sass"),
+        },
+      }
+    },
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
